115eac7e9c4b0ef2139d1f5b5d7b68db4cd58161
jsladerman <jsladerman@gmail.com>
2020-07-16 20:54:58 -0500

    Beginning merge

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,7 +1,5 @@
 import React, {Component} from 'react';
-import Card from 'react-bootstrap/Card';
 import styles from './css/Home.css';
-import Modal from '@trendmicro/react-modal';
 import '@trendmicro/react-modal/dist/react-modal.css';
 import {Auth} from 'aws-amplify';
 import {AmplifyAuthenticator, AmplifySignIn,} from '@aws-amplify/ui-react';
@@ -13,45 +11,21 @@ class Home extends Component {
         super(props);
         this.state = {
             signedIn: false,
-            showModal: false
         }
     }
 
     render() {
         console.log("Signed in: " + this.state.signedIn);
-        console.log(this.state.showModal);
         return (
             <div className="sign-in-div">
-                {/*<Card style={{ width: '18rem' }}>*/}
-                {/*    /!*<Card.Img variant="top" src="holder.js/100px180" />*!/*/}
-                {/*    <Card.Body>*/}
-                {/*        <Card.Title>Card Title</Card.Title>*/}
-                {/*        <Card.Text>*/}
-                {/*            Some quick example text to build on the card title and make up the bulk of*/}
-                {/*            the card's content.*/}
-                {/*        </Card.Text>*/}
-                {/*    </Card.Body>*/}
-                {/*</Card>*/}
                 <h2 className="title-message">Welcome back to CleanView</h2>
                 <p className="subtitle-message">Log in to launch your management dashboard</p>
-
-                <Modal style={{borderRadius:'200px'}} show={this.state.showModal}
-                       onClose={this.showModal} showCloseButton={false}>
-                    <ManagedLocationList/>
-                </Modal>
-                <button onClick={this.showModal}
-                        style={{visibility: this.state.showModal ? 'hidden' : 'visible'}}>Test Modal
-                </button>
-
                 <div className="sign-in">
                     <AmplifyAuthenticator>
                         <AmplifySignIn headerText="Sign in to your account"
                                     slot="sign-in" onFormSubmit={this.onSignInSubmit}/>
                         <button onClick={this.signOut}>Sign out</button>
                     </AmplifyAuthenticator>
-                </div>
-                <div style={{visibility: this.state.signedIn ? 'visible' : 'hidden'}}>
-                    <ManagedLocationInfo/>
                 </div>
             </div>
         );
@@ -62,7 +36,7 @@ class Home extends Component {
             Auth.signOut()
                 .then(response => {
                     console.log(response);
-                    this.setState({signedIn: false, showModal: false});
+                    this.setState({signedIn: false});
                 });
         } catch (error) {
             console.log('error signing out: ', error);
@@ -82,13 +56,6 @@ class Home extends Component {
             1500
         );
     };
-
-    showModal = () => {
-        this.setState({
-            showModal: !this.state.showModal
-        });
-    };
-
 }
 
 export default Home;
