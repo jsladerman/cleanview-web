4d6dfba0a0e894e06de49fd143fb55dcad0a5932
evansegaul <evan.segaul@vanderbilt.edu>
2020-07-19 19:40:21 -0400

    generic pie chart

--- src/Testing.js
+++ src/Testing.js
@@ -1,17 +1,28 @@
 import React, {Component} from 'react';
 import BarChart from './Components/ManagedLocationPage/BarChart';
+import BinaryPieChart from './Components/ManagedLocationPage/BinaryPieChart';
+
 
 class Testing extends Component {
     render () {
         return(
             <div>
-                <h1>Bar chart</h1>
+            <h1>Bar chart</h1>
             <BarChart width={500} 
-            height={400} 
-            titleText='Customer Ratings (by %)'
-            titleSize='12px' 
-            color='blue'/>
-
+                height={400} 
+                titleText='Customer Ratings (by %)'
+                titleSize='12px' 
+                color='#30B3CA'
+            />
+            <h2>Pie Chart</h2>
+            <BinaryPieChart 
+                height={400}
+                width={400}
+                titleText='Are your employees wearing masks?'
+                titleSize='12px'
+                margin = {30}
+                yesPct = {.15}
+            />
             </div>
         )
     }
