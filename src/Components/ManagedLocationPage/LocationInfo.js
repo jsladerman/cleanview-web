c736e54c73247cb8a74027335a88c0bd842c6970
Gabi Gresenz <gabriela.gresenz@gmail.com>
2020-07-27 18:47:55 -0400

    Styled QR Code and Menu Manager

--- src/Components/ManagedLocationPage/LocationInfo.js
+++ src/Components/ManagedLocationPage/LocationInfo.js
@@ -1,12 +1,9 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import {API, Storage} from 'aws-amplify';
-import { Auth } from 'aws-amplify';
-import './css/LocationInfo.css'
-import Container from 'react-bootstrap/Container'
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
-import Card from 'react-bootstrap/Card'
+import './css/LocationInfo.css';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Card from 'react-bootstrap/Card';
 
 class LocationInfo extends Component{
     constructor(props){
@@ -24,7 +21,7 @@ class LocationInfo extends Component{
     render(){
         return(
         <div id='location-info-wrapper'>
-            <h2>{this.state.data[0].loc_name}</h2>
+            <h2>{this.props.data.loc_name}</h2>
 
             <Container fluid>
                 <Row>
@@ -33,15 +30,15 @@ class LocationInfo extends Component{
                     </Col>
 
                     <Col>
-                        <p><span className="location-info-field-name">Business Type: </span>{this.state.data[0].loc_type}</p>
+                        <p><span className="location-info-field-name">Business Type: </span>{this.props.data.loc_type}</p>
                         {/* TODO:
                             This should have the specific email and phone number for that location.
                             Include this in the database.
                         */}
                         <p><span className="location-info-field-name">Email: </span>example@email.com</p>
                         <p><span className="location-info-field-name">Phone: </span>123-456-7890</p>
-                        <p><span className="location-info-field-name">Address:</span> {this.state.data[0].addr_line_1} {this.state.data[0].addr_line_2}</p>
-                        <p id = "address-line-2-field">{this.state.data[0].addr_city}, {this.state.data[0].addr_state}</p>
+                        <p><span className="location-info-field-name">Address:</span> {this.props.data.addr_line_1} {this.props.data.addr_line_2}</p>
+                        <p id = "address-line-2-field">{this.props.data.addr_city}, {this.props.data.addr_state}</p>
                     </Col>
                 </Row>
 
@@ -50,9 +47,9 @@ class LocationInfo extends Component{
                         {/* COVID-19 practices */}
                         <h5>Your current COVID-19 health practices</h5>
                         <div>
-                            <p> <span className="location-info-field-name">Are employees wearing masks? </span>{this.state.data[0].cleaning_practices.employee_masks}</p>
-                            <p> <span className="location-info-field-name">Are tables 6 feet apart? </span>{this.state.data[0].cleaning_practices.social_distancing}</p>
-                            <p> <span className="location-info-field-name">Can customers dine-in? </span>{this.state.data[0].cleaning_practices.dining_in}</p>
+                            {/* <p> <span className="location-info-field-name">Are employees wearing masks? </span>{this.props.data.cleaning_practices.employee_masks}</p>
+                            <p> <span className="location-info-field-name">Are tables 6 feet apart? </span>{this.props.data.cleaning_practices.social_distancing}</p>
+                            <p> <span className="location-info-field-name">Can customers dine-in? </span>{this.props.data.cleaning_practices.dining_in}</p> */}
                         </div>
                     </Col>
                 </Row>
