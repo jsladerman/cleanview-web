2c695303a47dbae9d4519632a5314a257c5e56de
evansegaul <evan.segaul@vanderbilt.edu>
2020-07-28 11:28:26 -0400

    QR code fix

--- src/Components/ManagedLocationPage/QRCodeGenerator.js
+++ src/Components/ManagedLocationPage/QRCodeGenerator.js
@@ -12,7 +12,8 @@ class QRCodeGenerator extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      sublocations: ''
+      sublocations: '',
+      environmentURL: ''
     };
 
     this.addSublocation = this.addSublocation.bind(this)
@@ -68,6 +69,7 @@ class QRCodeGenerator extends Component {
       .then(response => {
         let currState = this.state
         currState.sublocations = response.data.body.sublocations
+        currState.environmentURL = response.data.environmentURL
         this.setState(currState)
       })
       .catch(error => {
@@ -82,8 +84,8 @@ class QRCodeGenerator extends Component {
 
     const subLocationList = this.state.sublocations.map((sublocation) => {
       const total_id = this.props.id + '99strl99strl' + sublocation.id
-      return (
-        <SublocationQRCode id={total_id} name={sublocation.name} color={sublocation.color} key={sublocation.id} />
+      return(
+        <SublocationQRCode id={total_id} name={sublocation.name} color={sublocation.color} key={sublocation.id} environmentURL={this.state.environmentURL}/>
       )
     })
     return (
@@ -173,7 +175,8 @@ class SublocationQRCode extends Component {
   };
 
   render() {
-    const apiEndpoint = "https://api.qrserver.com/v1/create-qr-code/?data=https://inv6tn1p09.execute-api.us-east-1.amazonaws.com/dev/survey/"
+    
+    const apiEndpoint = "https://api.qrserver.com/v1/create-qr-code/?data=" + this.props.environmentURL + "/survey/"
     const sublocationId = this.props.id
     const colorParam = "&color=" + this.props.color
     const browserSizeParam = "&size=100x100"
