717387bfcb56428496dcd4ba3b06b360f9856246
cleanview-jake <67671111+cleanview-jake@users.noreply.github.com>
2020-08-05 17:11:42 -0400

    Jake/settings (#85)
    * backend support
    
    * amplify
    
    * Basic create settings progress
    
    * Initial settings page on sign up
    
    * Create manager settings works after making an account
    
    * Finished settings account creation and update in settings tab
    Added bootstrap error message instead of alert
    
    * Small star rating fix
    
    * Fixed bug from merging
    
    * Change password added to settings
    Generalized settings error and confirmation functions since there are now multiple forms
    
    * Change email works, need to add functionality for verify
    Consolidated all settings css into one file
    
    * Changing email and verifying should all work
    
    * Quick bug fix
    
    * Error handling
    
    * Get auth data again after verifying
    
    * Get auth data again after verifying
    
    * Error handling
    
    * Bugs and such
    
    * Bugs and such
    
    Co-authored-by: evansegaul <evan.segaul@vanderbilt.edu>
    Co-authored-by: jsladerman <jsladerman@gmail.com>

--- src/Pages/Settings.js
+++ src/Pages/Settings.js
@@ -1,15 +1,25 @@
 import React, {Component} from 'react';
 import styles from './css/Settings.module.css';
-import {API} from 'aws-amplify'
+import {Auth, API} from 'aws-amplify'
 import Alert from 'react-bootstrap/Alert'
 import SettingsBox from "../Components/Dashboard/SettingsBox";
+import ChangePasswordBox from "../Components/Settings/ChangePasswordBox";
+import ChangeEmailBox from "../Components/Settings/ChangeEmailBox";
 
 class Settings extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            updateSuccess: false,
-            phoneNumError: false
+            authInfo: this.props.authInfo,
+            updateSettingsSuccess: false,
+            updatePasswordSuccess: false,
+            settingsError: false,
+            passwordError: false,
+            passwordErrorMsg: '',
+            updateEmailSuccess: false,
+            emailError: false,
+            emailErrorMsg: '',
+            emailVerified: true
         }
     }
 
@@ -24,17 +34,50 @@ class Settings extends Component {
         return (
             <div>
                 <div className={styles.body}>
-                    {this.renderSuccessAlert()}
-                    {this.renderErrorAlert()}
+                    {this.state.updateSettingsSuccess ?
+                        this.renderSuccessAlert('Information Updated Successfully') : null}
+                    {this.state.settingsError ?
+                        this.renderErrorAlert('Phone number must be in this format: 800-555-1234') : null}
                     <div className={styles.settingsBox}>
                         <h3>Your Information</h3>
                         <div className={styles.separator}/>
                         <br/>
                         <SettingsBox
-                            authInfo={this.props.authInfo}
+                            authInfo={this.state.authInfo}
                             settingsInfo={this.props.settingsInfo}
                             submitFunc={this.updateSettings}
-                            phoneNumErrorFunc={this.triggerErrorAlert}
+                            errorFunc={this.triggerPhoneNumErrorAlert}
+                        />
+                    </div>
+                    {this.state.updateEmailSuccess ?
+                        this.renderSuccessAlert(
+                            'Email Address Updated Successfully',
+                            'Please check inbox for confirmation code.') : null}
+                    {this.state.emailError ?
+                        this.renderErrorAlert(this.state.emailErrorMsg) : null}
+                    <div className={styles.emailBox}>
+                        <h3>Change Email Address</h3>
+                        <div className={styles.separator}/>
+                        <br/>
+                        <ChangeEmailBox
+                            authInfo={this.state.authInfo}
+                            successFunc={() => {
+                                this.setState({updateEmailSuccess: true})
+                            }}
+                            errorFunc={this.triggerEmailErrorAlert}
+                        />
+                    </div>
+                    {this.state.updatePasswordSuccess ?
+                        this.renderSuccessAlert('Password Updated Successfully') : null}
+                    {this.state.passwordError ?
+                        this.renderErrorAlert(this.state.passwordErrorMsg) : null}
+                    <div className={styles.passwordBox}>
+                        <h3>Change Password</h3>
+                        <div className={styles.separator}/>
+                        <br/>
+                        <ChangePasswordBox
+                            submitFunc={this.updatePassword}
+                            errorFunc={this.triggerPasswordErrorAlert}
                         />
                     </div>
                 </div>
@@ -44,6 +87,7 @@ class Settings extends Component {
     }
 
     updateSettings = (params) => {
+        this.setState({updateSettingsSuccess: false, settingsError: false})
         let apiName = 'AccountSettingsApi'
         let path = '/account/manager'
         const requestData = {
@@ -52,9 +96,9 @@ class Settings extends Component {
             // plz include all of below parameters
             // if u need more or less, lmk and we can change the patch lambda together
             body: {
-                id: this.props.authInfo.username, // this is to identify the record
-                email: this.props.authInfo.attributes.email, // the rest are for updating
-                username: this.props.authInfo.username,
+                id: this.state.authInfo.username, // this is to identify the record
+                email: this.state.authInfo.attributes.email, // the rest are for updating
+                username: this.state.authInfo.username,
                 firstName: params.firstName,
                 lastName: params.lastName,
                 phone: params.phone
@@ -63,38 +107,64 @@ class Settings extends Component {
 
         API.patch(apiName, path, requestData)
             .then(() => {
-                this.setState({updateSuccess: true, phoneNumError: false})
-                this.props.getSettingsFunc(this.props.authInfo.username)
+                this.setState({updateSettingsSuccess: true, settingsError: false})
+                this.props.getSettingsFunc(this.state.authInfo.username)
             })
             .catch(error => {
-                console.log("Error: " + error)
+                this.triggerEmailErrorAlert(error.message);
             })
     }
 
-    renderSuccessAlert = () => {
-        if (this.state.updateSuccess)
-            return (
-                <Alert variant="success" dismissible
-                       onClose={() => this.setState({updateSuccess: false})}
-                       style={{whiteSpace: 'normal'}}>
-                    <Alert.Heading>Information Updated Successfully</Alert.Heading>
-                </Alert>
-            )
+    updatePassword = (params) => {
+        this.setState({updatePasswordSuccess: false, passwordError: false});
+        Auth.currentAuthenticatedUser()
+            .then(user => {
+                Auth.changePassword(user, params.oldPassword, params.newPassword)
+                    .then(() => this.setState({updatePasswordSuccess: true, passwordError: false}))
+                    .catch(() => this.triggerPasswordErrorAlert('Old Password is Incorrect'))
+            })
+            .catch(() => this.triggerPasswordErrorAlert('Old Password is Incorrect'))
     }
 
-    triggerErrorAlert = () => {
-        this.setState({phoneNumError: true, updateSuccess: false});
+    renderSuccessAlert = (msg, subMsg) => {
+        return (
+            <Alert variant="success" dismissible
+                   onClose={() => this.setState({
+                       updateSettingsSuccess: false,
+                       updateEmailSuccess: false,
+                       updatePasswordSuccess: false
+                   })}
+                   style={{whiteSpace: 'normal'}}>
+                <Alert.Heading>{msg}</Alert.Heading>
+                <div>
+                    {subMsg ? subMsg : null}
+                </div>
+            </Alert>
+        )
     }
 
-    renderErrorAlert = () => {
-        if (this.state.phoneNumError)
+    triggerPhoneNumErrorAlert = () => {
+        this.setState({settingsError: true, updateSettingsSuccess: false});
+    }
+
+    triggerEmailErrorAlert = (msg) => {
+        console.log(msg)
+        this.setState({emailErrorMsg: msg, emailError: true, updateEmailSuccess: false});
+    }
+
+    triggerPasswordErrorAlert = (msg) => {
+        this.setState({passwordErrorMsg: msg, passwordError: true, updatePasswordSuccess: false});
+    }
+
+    renderErrorAlert = (msg) => {
+        if (this.state.settingsError || this.state.emailError || this.state.passwordError)
             return (
                 <Alert variant="danger" dismissible
-                       onClose={() => this.setState({phoneNumError: false})}
+                       onClose={() => this.setState({settingsError: false, emailError:false, passwordError: false})}
                        style={{whiteSpace: 'normal'}}>
                     <Alert.Heading>Error</Alert.Heading>
                     <div>
-                        Phone number must be in this format: 800-555-1234
+                        {msg}
                     </div>
                 </Alert>
             )
