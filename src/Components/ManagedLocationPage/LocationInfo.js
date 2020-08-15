9da6add997a11b00c1cf427912bc60c1b29e88a5
jsladerman <jsladerman@gmail.com>
2020-08-15 04:27:43 -0500

    Made back button work Made edit button work independent of tabs Location info updates automatically to reflect edit

--- src/Components/ManagedLocationPage/LocationInfo.js
+++ src/Components/ManagedLocationPage/LocationInfo.js
@@ -1,301 +1,234 @@
-import React, { Component } from "react";
-import EditLocationInfo from "./EditLocationInfo";
+import React, {Component} from "react";
 import styles from "./css/LocationInfo.module.css";
 import Container from "react-bootstrap/Container";
-import Modal from "@trendmicro/react-modal";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Card from "react-bootstrap/Card";
-import API from "@aws-amplify/api";
-import { Redirect } from "react-router-dom";
-import {BsPencil} from "react-icons/bs";
+import {Redirect} from "react-router-dom";
 
 class LocationInfo extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      redirect: null,
-      data: this.props.data,
-      showModal: false,
-    };
-    this.handleUpdate = this.handleUpdate.bind(this);
-    this.loadData = this.loadData.bind(this);
-  }
-
-  handleUpdate = async () => {
-    this.toggleModal();
-    this.props.handleUpdate();
-    this.loadData();
-  };
-
-  handleDelete = async () => {
-    this.toggleModal();
-    this.props.handleUpdate();
-    this.setState({ redirect: '/home/locations' });
-  }
-
-  loadData = () => {
-    const apiName = "ManageLocationApi";
-    const path = "/location/object";
-    const requestData = {
-      headers: {},
-      response: true,
-      queryStringParameters: {
-        id: this.props.data.id,
-      },
-    };
-
-    API.get(apiName, path, requestData)
-      .then((response) => {
-        let currState = this.state;
-        currState.data = response.data.body;
-        this.setState(currState);
-      })
-      .catch((error) => {
-        console.log("Error: " + error);
-      });
-  };
-
-  toggleModal = () => {
-    this.setState({
-      showModal: !this.state.showModal,
-    });
-  };
-
-  covidResponseField = (
-    labelText,
-    valueText,
-    titleStyleClassName,
-    dataStyleClassName
-  ) => {
-    return (
-      <p className={dataStyleClassName}>
-        <span className={titleStyleClassName}>{labelText + " "} </span>{" "}
-        {valueText}
-      </p>
-    );
-  };
-
-  intToYN = (i) => {
-    if (!i) {
-      return "No";
+    constructor(props) {
+        super(props);
+        this.state = {
+            redirect: null,
+            data: this.props.data,
+        };
     }
-    return "Yes";
-  };
 
-  formatBusinessType = () => {
-    let business_type_format = "";
-    if (this.state.data.loc_type.length === 1) {
-      business_type_format = this.state.data.loc_type[0].toUpperCase();
-    } else if (this.state.data.loc_type) {
-      business_type_format =
-        this.state.data.loc_type[0].toUpperCase() +
-        this.state.data.loc_type.slice(1);
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        if (prevProps.data !== this.props.data)
+            this.setState({data: this.props.data});
     }
-    return business_type_format;
-  };
 
-  formatPhone = (n) => {
-    const area = n.substring(0, 3);
-    const secondSet = n.substring(3, 6);
-    const thirdSet = n.substring(6);
-    const res = "(" + area + ") " + secondSet + "-" + thirdSet;
-    return res;
-  };
+    render() {
+        if (this.state.redirect) {
+            return <Redirect push to={this.state.redirect}/>
+        }
+        const businessType = this.formatBusinessType();
+        const businessEmail = this.state.data.email;
+        const businessPhone = this.state.data.phone;
 
-  ynCharToString = (c) => {
-    if (c === "y") {
-      return "Yes";
-    } else if (c === "n") {
-      return "No";
-    }
-    return c;
-  };
+        const imgSrc =
+            this.state.data.imageUrl != null
+                ? this.state.data.imageUrl
+                : require("../../images/exampleRestaurant.png");
 
-  render() {
-    if (this.state.redirect) {
-      return <Redirect push to={this.state.redirect} />
-    }
-    const businessType = this.formatBusinessType();
-    const businessEmail = this.state.data.email;
-    const businessPhone = this.state.data.phone;
+        const covidResponseFieldDataCol1 = [
+            {
+                text: "Are your employees required to wear masks?",
+                value: this.ynCharToString(
+                    this.state.data.covidResponseSurvey.employeeMasks
+                ),
+                titleStyleClass: styles.covidResponseFieldTitle,
+                dataStyleClass: styles.covidResponseFieldData,
+            },
+            {
+                text: "Do you enforce social distancing guidelines?",
+                value: this.ynCharToString(
+                    this.state.data.covidResponseSurvey.socialDistancing
+                ),
+                titleStyleClass: styles.covidResponseFieldTitle,
+                dataStyleClass: styles.covidResponseFieldData,
+            },
+            {
+                text: "Do you sanitize tables after every meal?",
+                value: this.ynCharToString(
+                    this.state.data.covidResponseSurvey.sanitizeTables
+                ),
+                titleStyleClass: styles.covidResponseFieldTitle,
+                dataStyleClass: styles.covidResponseFieldData,
+            },
+            {
+                text: "Do you take the temperature of your employees every day?",
+                value: this.ynCharToString(
+                    this.state.data.covidResponseSurvey.employeeTemp
+                ),
+                titleStyleClass: styles.covidResponseFieldTitle,
+                dataStyleClass: styles.covidResponseFieldData,
+            },
+        ];
 
-    const imgSrc =
-      this.state.data.imageUrl != null
-        ? this.state.data.imageUrl
-        : require("../../images/exampleRestaurant.png");
+        const covidResponseFieldDataCol2 = [
+            {
+                text: "Do you have outside seating?",
+                value: this.ynCharToString(
+                    this.state.data.covidResponseSurvey.outsideSeating
+                ),
+                titleStyleClass: styles.covidResponseFieldTitle,
+                dataStyleClass: styles.covidResponseFieldData,
+            },
+            {
+                text: "What is your indoor capacity?",
+                value: this.state.data.covidResponseSurvey.indoorCapacity,
+                titleStyleClass: styles.covidResponseFieldTitle,
+                dataStyleClass: styles.covidResponseFieldData,
+            },
+            {
+                text: "What is your outdoor capacity (if applicable)?",
+                value:
+                    this.state.data.covidResponseSurvey.outdoorCapacity === ""
+                        ? "N/A"
+                        : this.state.data.covidResponseSurvey.outdoorCapacity,
+                titleStyleClass: styles.covidResponseFieldTitle,
+                dataStyleClass: styles.covidResponseFieldData,
+            },
+        ];
 
-    const covidResponseFieldDataCol1 = [
-      {
-        text: "Are your employees required to wear masks?",
-        value: this.ynCharToString(
-          this.state.data.covidResponseSurvey.employeeMasks
-        ),
-        titleStyleClass: styles.covidResponseFieldTitle,
-        dataStyleClass: styles.covidResponseFieldData,
-      },
-      {
-        text: "Do you enforce social distancing guidelines?",
-        value: this.ynCharToString(
-          this.state.data.covidResponseSurvey.socialDistancing
-        ),
-        titleStyleClass: styles.covidResponseFieldTitle,
-        dataStyleClass: styles.covidResponseFieldData,
-      },
-      {
-        text: "Do you sanitize tables after every meal?",
-        value: this.ynCharToString(
-          this.state.data.covidResponseSurvey.sanitizeTables
-        ),
-        titleStyleClass: styles.covidResponseFieldTitle,
-        dataStyleClass: styles.covidResponseFieldData,
-      },
-      {
-        text: "Do you take the temperature of your employees every day?",
-        value: this.ynCharToString(
-          this.state.data.covidResponseSurvey.employeeTemp
-        ),
-        titleStyleClass: styles.covidResponseFieldTitle,
-        dataStyleClass: styles.covidResponseFieldData,
-      },
-    ];
+        const covidResponseFieldsCol1 = covidResponseFieldDataCol1.map((field) =>
+            this.covidResponseField(
+                field.text,
+                field.value,
+                field.titleStyleClass,
+                field.dataStyleClass
+            )
+        );
 
-    const covidResponseFieldDataCol2 = [
-      {
-        text: "Do you have outside seating?",
-        value: this.ynCharToString(
-          this.state.data.covidResponseSurvey.outsideSeating
-        ),
-        titleStyleClass: styles.covidResponseFieldTitle,
-        dataStyleClass: styles.covidResponseFieldData,
-      },
-      {
-        text: "What is your indoor capacity?",
-        value: this.state.data.covidResponseSurvey.indoorCapacity,
-        titleStyleClass: styles.covidResponseFieldTitle,
-        dataStyleClass: styles.covidResponseFieldData,
-      },
-      {
-        text: "What is your outdoor capacity (if applicable)?",
-        value:
-          this.state.data.covidResponseSurvey.outdoorCapacity === ""
-            ? "N/A"
-            : this.state.data.covidResponseSurvey.outdoorCapacity,
-        titleStyleClass: styles.covidResponseFieldTitle,
-        dataStyleClass: styles.covidResponseFieldData,
-      },
-    ];
+        const covidResponseFieldsCol2 = covidResponseFieldDataCol2.map((field) =>
+            this.covidResponseField(
+                field.text,
+                field.value,
+                field.titleStyleClass,
+                field.dataStyleClass
+            )
+        );
 
-    const covidResponseFieldsCol1 = covidResponseFieldDataCol1.map((field) =>
-      this.covidResponseField(
-        field.text,
-        field.value,
-        field.titleStyleClass,
-        field.dataStyleClass
-      )
-    );
-
-    const covidResponseFieldsCol2 = covidResponseFieldDataCol2.map((field) =>
-      this.covidResponseField(
-        field.text,
-        field.value,
-        field.titleStyleClass,
-        field.dataStyleClass
-      )
-    );
-
-    const businessInformation = (
-      <div className={styles.locationInfoFields}>
-        <p>
-          <span className={styles.locationInfoFieldName}>Business Type: </span>{" "}
-          {businessType}
-        </p>
-        <p>
-          <span className={styles.locationInfoFieldName}>Email: </span>{" "}
-          {businessEmail}
-        </p>
-        <p>
-          <span className={styles.locationInfoFieldName}>Phone: </span>{" "}
-          {this.formatPhone(businessPhone)}
-        </p>
-        {/* TODO: Format below so it's aligned with the rest
+        const businessInformation = (
+            <div className={styles.locationInfoFields}>
+                <p>
+                    <span className={styles.locationInfoFieldName}>Business Type: </span>{" "}
+                    {businessType}
+                </p>
+                <p>
+                    <span className={styles.locationInfoFieldName}>Email: </span>{" "}
+                    {businessEmail}
+                </p>
+                <p>
+                    <span className={styles.locationInfoFieldName}>Phone: </span>{" "}
+                    {this.formatPhone(businessPhone)}
+                </p>
+                {/* TODO: Format below so it's aligned with the rest
                   Also format the text so the first letter of city is capital*/}
-        <p>
-          <span className={styles.locationInfoFieldName}>Address: </span>
-          {this.state.data.addrLine1}
-          {" " + this.state.data.addrLine2}
-          <br />
-          <span className={styles.addrBlock}>
+                <p>
+                    <span className={styles.locationInfoFieldName}>Address: </span>
+                    {this.state.data.addrLine1}
+                    {" " + this.state.data.addrLine2}
+                    <br/>
+                    <span className={styles.addrBlock}>
             {this.state.data.addrCity}, {this.state.data.addrState},{" "}
-            {this.state.data.addrZip}
+                        {this.state.data.addrZip}
           </span>
-        </p>
-      </div>
-    );
+                </p>
+            </div>
+        );
 
-    return (
-      <div>
-        <Modal
-          show={this.state.showModal}
-          onClose={this.toggleModal}
-          showCloseButton={true}
-          style={{ borderRadius: "100px" }}
-        >
-          <EditLocationInfo
-            data={this.state.data}
-            handleUpdate={this.handleUpdate}
-            handleDelete={this.handleDelete}
-          />
-        </Modal>
-        <div className={styles.locationInfoWrapper}>
-          <Container fluid className={styles.locationInfoContainer}>
-            {/*<Row>*/}
-            {/*  <Col>*/}
-            {/*    <h2>{this.state.data.loc_name}*/}
-            {/*    <span onClick={this.toggleModal}><BsPencil className={styles.editButton}/></span>*/}
-            {/*    </h2>*/}
-            {/*  </Col>*/}
-            {/*</Row>*/}
-            {/*<Row>*/}
-            {/*  <h6 className={styles.subtitle}>*/}
-            {/*    This page is how your location will be displayed on our coming-soon consumer information platform.*/}
-            {/*  </h6>*/}
-            {/*</Row>*/}
-            <Row>
-              <Col>
-                <Card.Img
-                  variant="top"
-                  style={{ borderRadius: "8px" }}
-                  src={imgSrc}
-                />
-              </Col>
-              <Col className={styles.thirdColumn}>{businessInformation}</Col>
-              <Col>
-              </Col>
-            </Row>
-            <Row>
-              <Col>
-                <h5 className={styles.covidResponseTitle}>
-                  {" "}
-                  Your self-reported COVID-19 health practices:{" "}
-                </h5>
-              </Col>
-            </Row>
-            <Row className={styles.covidResponseRow}>
-              <Col md="auto">{covidResponseFieldsCol1}</Col>
-              <Col>{covidResponseFieldsCol2}</Col>
-              <Col> </Col>
-            </Row>
-            <Row>
-              <Col>
-                {" "}
-              </Col>
-            </Row>
-          </Container>
-        </div>
-      </div>
-    );
-  }
+        return (
+            <div className={styles.locationInfoWrapper}>
+                <Container fluid className={styles.locationInfoContainer}>
+                    <Row>
+                        <Col>
+                            <Card.Img
+                                variant="top"
+                                style={{borderRadius: "8px"}}
+                                src={imgSrc}
+                            />
+                        </Col>
+                        <Col className={styles.thirdColumn}>{businessInformation}</Col>
+                        <Col>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col>
+                            <h5 className={styles.covidResponseTitle}>
+                                {" "}
+                                Your self-reported COVID-19 health practices:{" "}
+                            </h5>
+                        </Col>
+                    </Row>
+                    <Row className={styles.covidResponseRow}>
+                        <Col md="auto">{covidResponseFieldsCol1}</Col>
+                        <Col>{covidResponseFieldsCol2}</Col>
+                        <Col> </Col>
+                    </Row>
+                    <Row>
+                        <Col>
+                            {" "}
+                        </Col>
+                    </Row>
+                </Container>
+            </div>
+        );
+    }
+
+    covidResponseField = (
+        labelText,
+        valueText,
+        titleStyleClassName,
+        dataStyleClassName
+    ) => {
+        return (
+            <p key={Math.random()} className={dataStyleClassName}>
+                <span className={titleStyleClassName}>{labelText + " "} </span>{" "}
+                {valueText}
+            </p>
+        );
+    };
+
+    intToYN = (i) => {
+        if (!i) {
+            return "No";
+        }
+        return "Yes";
+    };
+
+    formatBusinessType = () => {
+        let business_type_format = "";
+        if (this.state.data.loc_type.length === 1) {
+            business_type_format = this.state.data.loc_type[0].toUpperCase();
+        } else if (this.state.data.loc_type) {
+            business_type_format =
+                this.state.data.loc_type[0].toUpperCase() +
+                this.state.data.loc_type.slice(1);
+        }
+        return business_type_format;
+    };
+
+    formatPhone = (n) => {
+        const area = n.substring(0, 3);
+        const secondSet = n.substring(3, 6);
+        const thirdSet = n.substring(6);
+        const res = "(" + area + ") " + secondSet + "-" + thirdSet;
+        return res;
+    };
+
+    ynCharToString = (c) => {
+        if (c === "y") {
+            return "Yes";
+        } else if (c === "n") {
+            return "No";
+        }
+        return c;
+    };
+
 }
 
 export default LocationInfo;
--- src/Components/ManagedLocationPage/ManagedLocationInfo.js
+++ src/Components/ManagedLocationPage/ManagedLocationInfo.js
@@ -1,6 +1,5 @@
 import React, {Component} from 'react';
 import styles from './css/ManagedLocationInfo.module.css'
-import PropTypes from 'prop-types';
 import QRCodeGenerator from './QRCodeGenerator'
 import AnalyticsDashboard from './AnalyticsDashboard'
 import LocationInfo from './LocationInfo'
@@ -11,15 +10,19 @@ import MenuManager from './MenuManager';
 import {API} from 'aws-amplify';
 import Auth from '@aws-amplify/auth';
 import {BsPencil} from "react-icons/bs/index";
+import EditLocationInfo from "./EditLocationInfo";
+import Modal from "@trendmicro/react-modal";
 
 class ManagedLocationInfo extends Component {
     constructor(props) {
         super(props);
         this.state = {
             redirect: null,
+            showModal: false,
             tab: this.props.match.params.tab,
             data: []
         }
+        this.handleUpdate = this.handleUpdate.bind(this);
     }
 
     componentDidMount = () => {
@@ -44,56 +47,80 @@ class ManagedLocationInfo extends Component {
                 width='100%'
             />;
         }
-
         return (
-            <div style={{padding: '40px'}}>
-                <div>
-                    Locations
+            <div>
+                <Modal
+                    show={this.state.showModal}
+                    onClose={this.toggleModal}
+                    showCloseButton={true}
+                    style={{ borderRadius: "100px" }}
+                >
+                    <EditLocationInfo
+                        data={this.state.data}
+                        handleUpdate={this.handleUpdate}
+                        handleDelete={this.handleDelete}
+                    />
+                </Modal>
+                <div style={{padding: '35px 70px'}}>
+                    <div style={{cursor: 'pointer'}} onClick={() => this.setState({redirect: '/home/locations'})}>
+                        <img src={require('../../images/back-arrow.svg')}
+                             style={{
+                                 float: 'left',
+                                 margin: '1px 0 8px -33px',
+                                 height: '20px'
+                             }}
+                             alt=''/>
+                        Locations
+                    </div>
+                    <div style={{display: 'flex', margin: '5px 0'}}>
+                        <h2 style={{fontWeight: '900', fontFamily: 'Roboto, serif'}}>
+                            {this.state.data.loc_name + ' '}
+                            <span style={{fontWeight: '300'}}>
+                            | {this.state.data.addrCity}
+                        </span>
+                            <span style={{marginLeft: '20px'}} onClick={this.toggleModal}><BsPencil
+                                className={styles.editButton}/></span>
+                        </h2>
+                    </div>
+                    <Tabs className={styles.tabsWrapper} activeKey={this.state.tab} onSelect={this.setTabURL}
+                          unmountOnExit={true} variant='pills'>
+                        <Tab tabClassName={this.state.tab === 'info' ? styles.active : null}
+                             eventKey='info' title='Information'>
+                            <div style={{marginLeft: '-30px'}}>
+                                <LocationInfo
+                                    data={this.state.data}
+                                    handleUpdate={this.props.handleUpdate}/>
+                            </div>
+                        </Tab>
+                        <Tab tabClassName={this.state.tab === 'qr' ? styles.active : null}
+                             eventKey='qr' title='QR Code'>
+                            <div style={{marginLeft: '-30px'}}>
+                                <QRCodeGenerator
+                                    name={this.state.data.loc_name}
+                                    id={this.props.id}/>
+                            </div>
+                        </Tab>
+                        <Tab tabClassName={this.state.tab === 'analytics' ? styles.active : null}
+                             eventKey='analytics' title='Analytics' unmountOnExit>
+                            <div style={{marginLeft: '-30px'}}>
+                                <AnalyticsDashboard
+                                    id={this.props.id}
+                                    sublocations={this.state.data.sublocations}/>
+                            </div>
+                        </Tab>
+                        <Tab tabClassName={this.state.tab === 'menu-manager' ? styles.active : null}
+                             eventKey='menu-manager' title='Menu Management'>
+                            <div style={{marginLeft: '-30px'}}>
+                                <MenuManager
+                                    id={this.props.id}
+                                    sublocations={this.state.data.sublocations}
+                                    menus={this.state.data.menus}
+                                    handleUpdate={this.setLocationInfo}
+                                />
+                            </div>
+                        </Tab>
+                    </Tabs>
                 </div>
-                <div style={{display: 'flex', margin: '5px 0'}}>
-                    <h2 style={{fontWeight: '900', fontFamily: 'Roboto, serif'}}>{this.state.data.loc_name}
-                        <span style={{marginLeft: '100px'}} onClick={this.toggleModal}><BsPencil
-                            className={styles.editButton}/></span>
-                    </h2>
-                </div>
-                <Tabs className={styles.tabsWrapper} activeKey={this.state.tab} onSelect={this.setTabURL}
-                      unmountOnExit={true} variant='pills'>
-                    <Tab tabClassName={this.state.tab === 'info' ? styles.active : null}
-                         eventKey='info' title='Information'>
-                        <div style={{marginLeft: '-30px'}}>
-                            <LocationInfo
-                                data={this.state.data}
-                                handleUpdate={this.props.handleUpdate}/>
-                        </div>
-                    </Tab>
-                    <Tab tabClassName={this.state.tab === 'qr' ? styles.active : null}
-                         eventKey='qr' title='QR Code'>
-                        <div style={{marginLeft: '-30px'}}>
-                            <QRCodeGenerator
-                                name={this.state.data.loc_name}
-                                id={this.props.id}/>
-                        </div>
-                    </Tab>
-                    <Tab tabClassName={this.state.tab === 'analytics' ? styles.active : null}
-                         eventKey='analytics' title='Analytics' unmountOnExit>
-                        <div style={{marginLeft: '-30px'}}>
-                            <AnalyticsDashboard
-                                id={this.props.id}
-                                sublocations={this.state.data.sublocations}/>
-                        </div>
-                    </Tab>
-                    <Tab tabClassName={this.state.tab === 'menu-manager' ? styles.active : null}
-                         eventKey='menu-manager' title='Menu Management'>
-                        <div style={{marginLeft: '-30px'}}>
-                            <MenuManager
-                                id={this.props.id}
-                                sublocations={this.state.data.sublocations}
-                                menus={this.state.data.menus}
-                                handleUpdate={this.setLocationInfo}
-                            />
-                        </div>
-                    </Tab>
-                </Tabs>
             </div>
         );
     }
@@ -112,43 +139,34 @@ class ManagedLocationInfo extends Component {
                 if (user == null)
                     this.setState({redirect: '/login'});
                 else
-                    this.getLocations(user.username);
+                    this.loadData();
             })
             .catch(error => {
                 console.log('Error: ' + error)
             });
     };
 
-    getLocations = (managerName) => {
-        const apiName = 'ManageLocationApi';
-        const path = '/location';
-        const myParams = {
+    loadData = () => {
+        const apiName = "ManageLocationApi";
+        const path = "/location/object";
+        const requestData = {
             headers: {},
             response: true,
             queryStringParameters: {
-                manager: managerName
+                id: this.props.id,
             },
         };
 
-        API.get(apiName, path, myParams)
-            .then(response => {
-                if (!this.findLocationFromArr(response['data'].data))
-                    this.setState({redirect: '/home'})
+        API.get(apiName, path, requestData)
+            .then((response) => {
+                let currState = this.state;
+                currState.data = response.data.body;
+                this.setState(currState);
             })
-            .catch(error => {
-                console.log('Error: ' + error)
-            })
+            .catch((error) => {
+                console.log("Error: " + error);
+            });
     };
-
-    findLocationFromArr = (locations) => {
-        for (const index in locations) {
-            if (locations[index]['id'] === this.props.id) {
-                this.setState({data: locations[index]});
-                return true;
-            }
-        }
-        return false;
-    }
 
     setTabURL = (tab) => {
         let newURL = this.props.location.pathname;
@@ -156,6 +174,24 @@ class ManagedLocationInfo extends Component {
         this.props.history.replace(newURL);
         this.setState({tab: tab});
     }
+
+    handleUpdate = async () => {
+        this.toggleModal();
+        this.props.handleUpdate();
+        this.loadData();
+    };
+
+    handleDelete = async () => {
+        this.toggleModal();
+        this.props.handleUpdate();
+        this.setState({ redirect: '/home/locations' });
+    }
+
+    toggleModal = () => {
+        this.setState({
+            showModal: !this.state.showModal,
+        });
+    };
 
 }
 
